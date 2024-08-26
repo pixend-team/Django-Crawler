036fad8c583dddbeaf8059dc8bd21ce71c0719e6
Hosein <hosein.nasiri@outlook.com>
2024-08-26 15:33:51 +0330

    DockerFile up

---
+++ entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+python manage.py migrate --no-input
+python manage.py collectstatic --no-input
+DJANGO_SUPERUSER_PASSWORD=$SUPER_USER_PASSWORD python manage.py createsuperuser --username $SUPER_USER_NAME --email $SUPER_USER_EMAIL --noinput
+
+exec "$@"
